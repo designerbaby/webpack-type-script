53415862d5ab7c0a6c4fe8b0aafcd36fcdce8e88
liujiana <liujiana@yy.com>
2020-03-21 22:50:20 +0800

    first commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+## webpack-type-script
+
+typescript在webpack中的打包配置示例文件
+
+- tsconfig.json typescript必备的配置文件
+- webpack.config.js 固有的配置文件
+- package.json 项目配置文件
+- src 源码
+- dist 目标代码
---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import * as _ from 'lodash'
+
+class Greeter {
+  greeting: string;
+  constructor(message: string) {
+    this.greeting = message;
+  }
+  greet() {
+    return _.join(["Hello,", '', this.greeting], '')
+    // return 'hello, ' + this.greeting
+  }
+}
+
+let greeter = new Greeter("hello")
+
+alert(greeter.greet())
+// let button = document.createElement('button')
+// button.textContent = 'Say Hello'
+// button.onclick = function () {
+//   alert(greeter.greet())
+// }
+
+// document.body.appendChild(button)
